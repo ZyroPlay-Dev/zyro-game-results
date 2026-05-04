d272828bed9ae0f6a0864e04df513fbb8cf43702
Zyro Play Studio <support.zyroplaystudio@gmail.com>
2026-05-04 06:38:35 +0530

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,12 +1,13 @@
 export default async function handler(req, res) {
 
-  const API_KEY = "zyro1008";
+  const API_KEY = process.env.API_KEY;
+  const search = req.query.search || "";
 
-  const response = await fetch(
-    "https://script.google.com/macros/s/AKfycbxdBZPdWRN6Nw28y4l_GTWYD_eziwDdmIjZgaRT-Q-a9QrzFOJlYerpE57AX9qM4Oxe2w/exec?key=" + API_KEY
-  );
+  const url = `https://script.google.com/macros/s/AKfycbxSJTWId-fKfR8C80zzzaBvf8MUwCP6wKegl_C5RZIAmP_cntdyx8CcBUjARLaW7u2JcQ/exec?key=${API_KEY}&search=${search}`;
 
+  const response = await fetch(url);
   const data = await response.json();
 
   res.status(200).json(data);
 }
+
